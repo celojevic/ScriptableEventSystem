0579ce41f7b220e6de3c848c0c7153545d749a94
gooby <stefancelojevic@gmail.com>
2021-05-25 14:26:39 -0400

    Added OnKeyPressTrigger

--- Assets/Scripts/Enums.cs
+++ Assets/Scripts/Enums.cs
@@ -4,6 +4,11 @@ public enum BoolOp
     True, False, Switch
 }
 
+public enum KeyPressType
+{
+    Down, Up, Hold
+}
+
 public enum MathOp
 {
     Add, Sub, Div, Mul
---
+++ Assets/Scripts/ScriptableEvents/Triggers/OnKeyPressTrigger.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+public class OnKeyPressTrigger : ScriptableEventTrigger
+{
+
+    [Header("Key Press Trigger")]
+    public KeyPressType PressType;
+    public KeyCode Key;
+
+    private void Update()
+    {
+        CheckKeyPresses();
+    }
+
+    void CheckKeyPresses()
+    {
+        switch (PressType)
+        {
+            case KeyPressType.Down:
+                HandleKeyDown();
+                break;
+
+            case KeyPressType.Up:
+                HandleKeyUp();
+                break;
+
+            case KeyPressType.Hold:
+                HandleKeyHeld();
+                break;
+        }
+    }
+
+    void HandleKeyDown()
+    {
+        if (Input.GetKeyDown(Key))
+            TriggerEvents();
+    }
+
+    void HandleKeyUp()
+    {
+        if (Input.GetKeyUp(Key))
+            TriggerEvents();
+    }
+
+    void HandleKeyHeld()
+    {
+        if (Input.GetKey(Key))
+            TriggerEvents();
+    }
+
+}
